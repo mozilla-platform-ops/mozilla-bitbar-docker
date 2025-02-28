a68d0a15dd9e9c9bfe8e45ee64a8c0d03bdb4a65
Andrew Erickson <aerickson@mozilla.com>
2025-02-28 13:39:03 -0800

    test: output junit format on cci

--- local_test.sh
+++ local_test.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# we want cleanup to run even if tests failed
-# - use true on test cmd
 set -e
 # set -x
 
@@ -13,6 +11,12 @@ set -e
 . ./vars.sh
 
 CONTAINER_NAME="devicepool-inspec-testing"
+# if container exists, stop and remove it
+# ignore errors if container doesn't exist
+if [ -n "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    docker stop $CONTAINER_NAME || true
+    docker rm $CONTAINER_NAME || true
+fi
 
 # if we're on CircleCI (CIRCLE_BRANCH is defined), do special stuff
 if [ -n "$CIRCLE_BRANCH" ]; then
@@ -21,6 +25,7 @@ else
     PLATFORM_ARG="--platform=$LINUX_PLAT"
 fi
 
+set -x
 docker run \
     --name $CONTAINER_NAME \
     $PLATFORM_ARG \
@@ -33,17 +38,20 @@ docker run \
     -e gecko_t_ap_test_g5="SECRET_SECRET_SECRET_DO NOT LEAK 1" \
     -e TESTDROID_APIKEY="SECRET_SECRET_SECRET_DO NOT LEAK 2" \
     -d -t devicepool
-
+set +x
 
 # if we're on CircleCI (CIRCLE_BRANCH is defined), do special stuff
 if [ -n "$CIRCLE_BRANCH" ]; then
-    inspec exec image_tests -t docker://$CONTAINER_NAME
+    set -x
+    # output to junit2 format for CircleCI
+    inspec exec image_tests -t docker://$CONTAINER_NAME --reporter cli junit2:.test_output/junit_output
 else
+    set -x
     # if we're running locally ignore the exit code so we can cleanup'
     # TODO: figure out a more elegant way of handling this
     inspec exec image_tests -t docker://$CONTAINER_NAME || true
 fi
 
-# tear down
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
+# # tear down
+# docker stop $CONTAINER_NAME
+# docker rm $CONTAINER_NAME
