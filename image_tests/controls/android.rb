b6365ed87e9bfd188015dcc2a75f457ae8c14093
Andrew Erickson <aerickson@mozilla.com>
2025-02-27 16:14:53 -0800

    improve test

--- image_tests/controls/android.rb
+++ image_tests/controls/android.rb
@@ -1,6 +1,39 @@
 title "android checks"
 
-describe bash('PATH=$PATH:/builds/worker/android-sdk-linux/platform-tools adb --version') do
+path_env = [
+  '/usr/local/sbin',
+  '/usr/local/bin',
+  '/usr/sbin',
+  '/usr/bin',
+  '/sbin',
+  '/bin',
+  '/builds/worker/bin',
+  '/usr/local/bin',
+  '/builds/worker/android-sdk-linux/platform-tools',
+  '/builds/worker/android-sdk-linux/platform-tools/bin',
+  '/builds/worker/android-sdk-linux/tools',
+  '/builds/worker/android-sdk-linux/tools/bin',
+].join(':')
+
+describe bash("PATH=#{path_env} adb --version") do
   its('exit_status') { should eq 0 }
   its('stdout') { should match /Android Debug Bridge version 1.0.41/ }
 end
+
+describe bash("PATH=#{path_env} fastboot --version") do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /fastboot version 35.0.2-12147458/ }
+end
+
+describe bash("PATH=#{path_env} sdkmanager --version") do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /24.3.4/ }
+end
+
+# describe bash("PATH=#{path_env} avdmanager --version") do
+#   its('exit_status') { should eq 0 }
+# end
+
+# describe bash("PATH=#{path_env} emulator -version") do
+#   its('exit_status') { should eq 0 }
+# end
