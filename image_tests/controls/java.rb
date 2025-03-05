48a462312060507876caaf11b38132288532ed0b
Andrew Erickson <aerickson@mozilla.com>
2025-03-04 16:10:22 -0800

    test refactoring

--- image_tests/controls/java.rb
+++ image_tests/controls/java.rb
@@ -1,20 +1,13 @@
 title "java checks"
 
-path_env = [
-  '/usr/bin',
-  '/bin',
-  '/usr/local/sbin',
-  '/usr/local/bin',
-  '/usr/sbin',
-  '/sbin',
-].join(':')
+source_bash_snippet = "source /root/.bashrc &&"
 
-describe bash("PATH=#{path_env} javac --version") do
+describe bash("#{source_bash_snippet} javac --version") do
   its('exit_status') { should eq 0 }
   its('stdout') { should match /17.0.14/ }
 end
 
-describe bash("PATH=#{path_env} java --version") do
+describe bash("#{source_bash_snippet} java --version") do
   its('exit_status') { should eq 0 }
   its('stdout') { should match /17.0.14/ }
 end
